a6f6346bab882c94036a1fd3be5a081be8ca256b
Maksym Fedorets <Tifer_Aiden@protonmail.com>
2022-09-27 21:01:27 +0200

    Add project files.

---
+++ ColdUniverse/Views/Windows/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace ColdUniverse
+{
+    /// <summary>
+    /// Логика MainWindow
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
