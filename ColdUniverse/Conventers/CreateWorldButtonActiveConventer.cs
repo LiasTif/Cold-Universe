35f79fea248db1ce8a71e60a06f49d11d2a35f00
Maksym Fedorets <Tifer_Aiden@protonmail.com>
2022-10-10 23:36:56 +0200

    ver 0.0.1

---
+++ ColdUniverse/Conventers/CreateWorldButtonActiveConventer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace ColdUniverse.Conventers
+{
+    public class CreateWorldButtonActiveConventer : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is String str && value != null)
+            {
+                // если названия нет, кнопка 'создать мир' не активна
+                if (str != "")
+                    return true;
+            }
+            return false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
