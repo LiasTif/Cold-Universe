812c2749f6947bdca3ee882b456a84116704b14f
Maksym Fedorets <Tifer_Aiden@protonmail.com>
2022-11-01 15:26:47 +0100

    Added IdSetter Class

---
+++ WorldGenerationDevelop/Models/WorldCreation/Generation/IdSetter.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.ObjectModel;
+
+namespace WorldGenerationDevelop.Models.WorldCreation.Generation
+{
+    public class IdSetter
+    {
+        public ObservableCollection<int> IdCollection { get; private set; }
+
+        public int GetId()
+        {
+            if (IdCollection == null)
+                IdCollection = new ObservableCollection<int> { 1 };
+            else
+                IdCollection.Add(IdCollection.Count + 1);
+
+            return IdCollection[IdCollection.Count - 1];
+        }
+    }
+}
