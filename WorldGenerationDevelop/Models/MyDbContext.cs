9d713f8395e70d5ab29b9f0f63b18cc0019379b0
Maksym Fedorets <Tifer_Aiden@protonmail.com>
2022-11-05 13:08:48 +0100

    Added Entity Framework

---
+++ WorldGenerationDevelop/Models/MyDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System.Data.Entity;
+using WorldGenerationDevelop.Models.WorldCreation;
+
+namespace WorldGenerationDevelop.Models
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext() : base("DbColdUniverseConnection")
+        {
+        }
+
+        public DbSet<Galaxy> Galaxies { get; set; }
+        public DbSet<StarSystem> StarSystems { get; set; }
+    }
+}
