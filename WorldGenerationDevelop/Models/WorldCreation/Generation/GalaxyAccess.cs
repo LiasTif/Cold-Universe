c311372b0878698f2c8395230fa529b088088c9d
Maksym Fedorets <Tifer_Aiden@protonmail.com>
2022-11-10 17:09:53 +0100

    Added Access classes to galaxy and Star System

---
+++ WorldGenerationDevelop/Models/WorldCreation/Generation/GalaxyAccess.cs
@@ -0,0 +1,25 @@
+﻿namespace WorldGenerationDevelop.Models.WorldCreation.Generation
+{
+    public class GalaxyAccess
+    {
+        /// <summary>
+        /// get information about galaxy
+        /// </summary>
+        public Galaxy GetGalaxy(int id)
+        {
+            using var context = new MyDbContext();
+            return context.Galaxies.Find(id);
+        }
+
+        /// <summary>
+        /// remove galaxy from db
+        /// </summary>
+        public void RemoveGalaxy(int id)
+        {
+            using var context = new MyDbContext();
+
+            context.Galaxies.Remove(context.Galaxies.Find(id));
+            context.SaveChanges();
+        }
+    }
+}
---
+++ WorldGenerationDevelop/Models/WorldCreation/Generation/StarSystemAccess.cs
@@ -0,0 +1,14 @@
+﻿namespace WorldGenerationDevelop.Models.WorldCreation.Generation
+{
+    public class StarSystemAccess
+    {
+        /// <summary>
+        /// get information about star system
+        /// </summary>
+        public StarSystem GetStarSystem(int id)
+        {
+            using var context = new MyDbContext();
+            return context.StarSystems.Find(id);
+        }
+    }
+}
